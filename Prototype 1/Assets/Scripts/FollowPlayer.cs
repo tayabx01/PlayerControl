94a8ca8687355083c94180195793004857ede2bd
xtayab <afrozet1@roverkids.org>
2023-12-08 13:37:34 -0500

    finish 1.3
    finished

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public GameObject Player;
+    private Vector3 offset = new Vector3 (0, 5, -7);
 
     // Start is called before the first frame update
     void Start()
@@ -13,10 +14,8 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        // we'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed );
-
+        transform position = player.transform.position + offset;
     }
 }
