fed466a80d715f649aa315fa82c1e8b7d9d192fc
xtayab <afrozet1@roverkids.org>
2023-12-12 13:35:19 -0500

    finish 1.4

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject Player;
+    public GameObject player;
     private Vector3 offset = new Vector3 (0, 5, -7);
 
     // Start is called before the first frame update
@@ -16,6 +16,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform position = player.transform.position + offset;
+        transform.position = player.transform.position + offset;
     }
 }
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
+        horizontalInput; = Input.GetAxis("Horizontal")
+        forardInput; = Input.GetAxis("Vertical")
+            
+            
+
         // we'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed );
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
 
     }
 }
